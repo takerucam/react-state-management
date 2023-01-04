61c139fe9833047e40923ee50970a71bbbfdca16
Takeru Miyake <m18kecam777@gmail.com>
2023-01-04 15:28:35 +0900

    🧹 _appを整理

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useState } from 'react'
-import { Todo } from 'types'
-
-const TODOS: Todo[] = [
-  { id: 1, text: 'foo', isDone: false },
-  { id: 2, text: 'bar', isDone: true },
-]
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [todos, setTodos] = useState<Todo[]>(TODOS)
   return (
     <>
       <main>
-        <Component {...pageProps} todos={todos} setTodos={setTodos} />
+        <Component {...pageProps} />
       </main>
     </>
   )
